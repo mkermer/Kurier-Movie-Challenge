aab5dda39685efc0ffe0e8f763bb3d988a7a28c7
Adrian Fabjanski <adrianfab19@gmail.com>
2021-02-22 13:41:24 +0100

    movie details raw page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
-import MovieOverview from './components/movie-overview';
-
+import "./App.css";
+import MovieOverview from "./components/movie-overview";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import MovieDetail from "./components/MovieDetail";
 
 function App() {
   return (
-    <div className="App">
-      <MovieOverview />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={MovieOverview} />
+          <Route path="/movie" component={MovieDetail} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
