ec3e08c271ca611045d9f30d3121ae9e61f7fb4c
jessy <jessynnebs@gmail.com>
2021-02-22 12:24:39 +0100

    login and registration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import React from "react";
 import './App.css';
+// import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+// import * as actions from '../../actions/app.action';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from './components/Login/Login';
+import Registration from './components/Registration/Registration';
+
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       
-    </div>
+              <Switch>
+                {/* <Route path="/" exact component={LandingPage}/> */}
+                <Route path="/Login" component={Login}/>
+                <Route path="/Registration" component={Registration}/>
+                
+            
+              </Switch>
+
+      </div>
+         </Router>  
   );
 }
 
-export default App;
+// const mapStateToProps = state => ({ applicationState: state });
+// const mapDispatchToProps = dispatch => ({ actions: bindActionCreators(actions, dispatch) });
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default App
